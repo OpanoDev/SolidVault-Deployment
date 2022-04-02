5b2c5e9d31d0034c6900f7e1976e9ddc4dc62350
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2022-04-02 16:28:25 +0530

    refactor(id_decorator): refractored code with id_decorator
    refracted portions where req.user is being used explicitly and sending the same dto to service with
    custom decorator (get_current_user)

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -11,25 +11,29 @@ import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { UserService } from '../services/user.service';
 import { ReqUserDto, UpdateUserDto } from '../dto';
+import { GetCurrentUserById } from 'src/auth/decorators/getuserid.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
   @Get('/me')
   @UseGuards(AuthGuard('jwt'))
-  userData(@Req() req: Request): string {
-    return this.userService.userData(req.user);
+  userData(@GetCurrentUserById() id: string): Promise<string> {
+    return this.userService.userData(id);
   }
 
   @Put('/update-me')
   @UseGuards(AuthGuard('jwt'))
-  updateUserData(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.updateUserData(updateUserDto, req.user);
+  updateUserData(
+    @GetCurrentUserById() id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.updateUserData(updateUserDto, id);
   }
 
   @Delete('/delete-me')
   @UseGuards(AuthGuard('jwt'))
-  deleteUser(@Req() req: Request) {
-    return this.userService.deleteUser(req.user);
+  deleteUser(@GetCurrentUserById() id: string) {
+    return this.userService.deleteUser(id);
   }
 }
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,20 +1,21 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from 'src/auth/models/user.schema';
-import { UpdateUserDto, ReqUserDto } from '../dto';
+import { UpdateUserDto } from '../dto';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
-  userData(reqUserDto: ReqUserDto): string {
-    return `Welcome ${reqUserDto.username}`;
+  async userData(id: string): Promise<string> {
+    const user: User = await this.userModel.findById(id);
+    return `Welcome ${user.fullName}`;
   }
 
-  async updateUserData(updateUserDto: UpdateUserDto, reqUserDto: ReqUserDto) {
+  async updateUserData(updateUserDto: UpdateUserDto, id: string) {
     const options = { new: true };
     const upUser = await this.userModel.findByIdAndUpdate(
-      reqUserDto.sub,
+      id,
       updateUserDto,
       options,
     );
@@ -23,8 +24,8 @@ export class UserService {
     return upUser;
   }
 
-  async deleteUser(reqUserDto: ReqUserDto) {
-    const user = await this.userModel.findByIdAndDelete(reqUserDto.sub);
+  async deleteUser(id: string) {
+    const user = await this.userModel.findByIdAndDelete(id);
     if (!user) throw new BadRequestException('User Not Found');
 
     return 'User Deleted';
